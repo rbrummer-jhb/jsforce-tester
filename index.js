9fd0f369a28f0f71d0661b61d7b23d62df3a8293
Rick Roesler <sfdx.developer@gmail.com>
2022-05-04 16:57:31 -0700

    add CustomLabel support

--- index.js
+++ index.js
@@ -6,14 +6,18 @@ const _ = require('lodash');
 const Bluebird = require('bluebird');
 
 const conn = new jsforce.Connection({ version: '52.0' });
+
+const util = require('util');
 const fs = require('fs');
-const parseString = require('xml2js').parseString;
+const stream = require('stream');
+const parseString = require('xml2js').parseStringPromise;
+const decompress = require('decompress');
+
+const finished = util.promisify(stream.finished);
 
 main()
 
 async function main () {
-console.log(username);
-console.log(password);
 	try {
 		await conn.login(username, password);
 	} catch (err) {
@@ -21,53 +25,96 @@ console.log(password);
 	}
 	
 	try {
-		// console.log(await (getSourceMember('0MZ8E000005uz3KWAQ')));
-		// Object.values((await getFieldInfo('Booking__c')).fields.custom).forEach(field => {
-		// 	if (field.calculatedFormula) console.log(`${field.developerName}: formula: ${field.calculatedFormula}`);
-		// 	if (field.referenceTo) console.log(`${field.developerName}: reference to: ${field.referenceTo}`);
-		// 	if (field.relationshipName) console.log(`${field.developerName}: relationship name: ${field.relationshipName}`);
-		// });
-		// const fields = (await getFieldInfo('My_Custom_Metadata_Type__mdt')).fields;
-		// console.log(JSON.stringify(fields, null, 2));
+		/* get CustomLabel summary info */
+		const customLabelSummary = await getCustomLabelSummary();
+		console.log(customLabelSummary);
+		const customLabelsXml = await getCustomLabelXml(['thank_you', 'greeting']);
+		console.log(customLabelsXml);
+		const customLabels = await parseString(customLabelsXml);
+		console.log(JSON.stringify(customLabels, null, 2));
+		
+		
+		/* get field info
+		Object.values((await getFieldInfo('Booking__c')).fields.custom).forEach(field => {
+			if (field.calculatedFormula) console.log(`${field.developerName}: formula: ${field.calculatedFormula}`);
+			if (field.referenceTo) console.log(`${field.developerName}: reference to: ${field.referenceTo}`);
+			if (field.relationshipName) console.log(`${field.developerName}: relationship name: ${field.relationshipName}`);
+		});
+		const fields = (await getFieldInfo('My_Custom_Metadata_Type__mdt')).fields;
+		console.log(JSON.stringify(fields, null, 2));
+		*/
+
+		/* get dashboard and report info
 		const dashboard = await getDashboard('Global_Sales_Dashboards/IhTVktDDdNKZpPdRzrGxIqgAydZyHH');
 		console.log(JSON.stringify(dashboard, null, 2));
 		const report = await getReport('unfiled$public/New_Accounts_Report_vvx');
 		console.log(JSON.stringify(report, null, 2));
 		const reportType = await getReportType();
 		console.log(JSON.stringify(reportType, null, 2));
-// console.log(await getReportType());
-		// const type = {
-		// 	members: ['Opportunity.ForecastCategoryName'],
-		// 	name: 'CustomField'
-		// };
+		*/
+
+		/* get field info using metadata api
+		const type = {
+			members: ['Opportunity.ForecastCategoryName'],
+			name: 'CustomField'
+		};
 	
-		// const params = {
-		// 	apiVersion: '52.0',
-		// 	unpackaged: {types: [type]}
-		// };
+		const params = {
+			apiVersion: '52.0',
+			unpackaged: {types: [type]}
+		};
 		
-		// const result = await conn.metadata.retrieve(params).stream().pipe(fs.createWriteStream('./MyPackage.zip'));
+		const result = await conn.metadata.retrieve(params).stream().pipe(fs.createWriteStream('./MyPackage.zip'));
 		
-		// const xml = fs.readFileSync('./OpportunityStage.standardValueSet').toString('utf8')
-		// console.log(xml);
-		// parseString(xml, function (err, result) {
-		// 	console.log(result.StandardValueSet.standardValue);
-			// 	const picklistFields = result.CustomObject.fields.filter(field => field.type && field.type[0] === 'Picklist');
-		// 	picklistFields.forEach(field => {
-		// 		if (field.valueSet && field.valueSet[0].valueSetDefinition) {
-		// 			console.log(field.valueSet[0].valueSetDefinition[0]);
-		// 			let s = JSON.stringify(field.valueSet[0].valueSetDefinition[0]);
-		// 			console.log(JSON.parse(s).sorted[0]);
-		// 		}
-		// 		// if (field.valueSet && field.valueSet[0].valueSetName) {
-		// 		// 	console.log(field.valueSet[0].valueSetName[0]);
-		// 		// }
-		// 		// if (!field.valueSet) console.log('StandardValueSet');
-		// 	});
-		// }); 
+		const xml = fs.readFileSync('./OpportunityStage.standardValueSet').toString('utf8')
+		console.log(xml);
+		parseString(xml, function (err, result) {
+			console.log(result.StandardValueSet.standardValue);
+				const picklistFields = result.CustomObject.fields.filter(field => field.type && field.type[0] === 'Picklist');
+			picklistFields.forEach(field => {
+				if (field.valueSet && field.valueSet[0].valueSetDefinition) {
+					console.log(field.valueSet[0].valueSetDefinition[0]);
+					let s = JSON.stringify(field.valueSet[0].valueSetDefinition[0]);
+					console.log(JSON.parse(s).sorted[0]);
+				}
+				// if (field.valueSet && field.valueSet[0].valueSetName) {
+				// 	console.log(field.valueSet[0].valueSetName[0]);
+				// }
+				// if (!field.valueSet) console.log('StandardValueSet');
+			});
+		}); 
+		*/
 	} catch (err) {
 		console.log(err)
 	}
+}
+
+async function getCustomLabelSummary() {
+	try {
+		return (await conn.metadata.list([{type: 'CustomLabel'}]))
+	} catch (err) {
+		console.log(err)
+	}
+}
+
+async function getCustomLabelXml(customLabelFullNames) {
+	const writable = fs.createWriteStream('temp.zip');
+
+	const type = {
+		members: customLabelFullNames,
+		name: 'CustomLabel'
+	};
+
+	const params = {
+		apiVersion: '52.0',
+		unpackaged: {types: [type]}
+	};
+	
+	await conn.metadata.retrieve(params).stream().pipe(writable);
+	await finished(writable);
+	await decompress('temp.zip', './');
+	fs.unlinkSync('temp.zip');
+	return fs.readFileSync('./unpackaged/labels/CustomLabels.labels').toString('utf8')
 }
 
 async function getDashboard(apiName) {
@@ -204,45 +251,48 @@ async function getObjectInfo (objectName) {
 
 async function getFieldInfo (objectName) {
 	try {
+		/* get field info from CustomObject metadata */
 		return await conn.metadata.read('CustomObject', [objectName]);
-		// const objectMetadataDescribe = await conn.describe(objectName);
+		
+		/* get field info using Describe methods
+		const objectMetadataDescribe = await conn.describe(objectName);
+		const fields = objectMetadataDescribe.fields.reduce(
+			(acc, field) => {
+				// if (field.name === 'Type') console.log(field);
+				const formattedField = {
+					picklistValues: field.picklistValues,
+					referenceTo: field.referenceTo,
+					developerName: field.name,
+					label: field.label,
+					relationshipName: field.relationshipName,
+					nillable: field.nillable,
+					type: field.type,
+					controllerName: field.controllerName,
+					defaultValue: field.defaultValue,
+					calculatedFormula: field.calculatedFormula,
+					inlineHelpText: field.inlineHelpText,
+					restrictedPicklist: field.restrictedPicklist
+				};
 
-		// const fields = objectMetadataDescribe.fields.reduce(
-		// 	(acc, field) => {
-		// 		// if (field.name === 'Type') console.log(field);
-		// 		const formattedField = {
-		// 			picklistValues: field.picklistValues,
-		// 			referenceTo: field.referenceTo,
-		// 			developerName: field.name,
-		// 			label: field.label,
-		// 			relationshipName: field.relationshipName,
-		// 			nillable: field.nillable,
-		// 			type: field.type,
-		// 			controllerName: field.controllerName,
-		// 			defaultValue: field.defaultValue,
-		// 			calculatedFormula: field.calculatedFormula,
-		// 			inlineHelpText: field.inlineHelpText,
-		// 			restrictedPicklist: field.restrictedPicklist
-		// 		};
+				const key = formattedField.developerName;
 
-		// 		const key = formattedField.developerName;
-
-		// 		if (field.name.endsWith('__c')) {
-		// 			acc.custom[key] = formattedField;
-		// 		} else {
-		// 			acc.standard[key] = formattedField;
-		// 		}
-		// 		return acc;
-		// 	},
-		// 	{
-		// 		standard: {},
-		// 		custom: {}
-		// 	}
-		// );
-		// return {
-		// 	objectMetadataDescribe,
-		// 	fields
-		// };
+				if (field.name.endsWith('__c')) {
+					acc.custom[key] = formattedField;
+				} else {
+					acc.standard[key] = formattedField;
+				}
+				return acc;
+			},
+			{
+				standard: {},
+				custom: {}
+			}
+		);
+		return {
+			objectMetadataDescribe,
+			fields
+		};
+		*/
 	} catch (err) {
 		console.log(err)
 	}
